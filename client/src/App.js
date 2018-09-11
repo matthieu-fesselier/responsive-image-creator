7700c76ce9b4b750cc9b87059311ad613015d511
Matthieu Fesselier <matthieu.fesselier@gmail.com>
2018-09-11 14:54:06 +0200

    :lipstick: Update theme

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,49 @@ import React, {Component} from 'react';
 import OriginalImageContainer from "./OriginalImageContainer";
 import SizesListContainer from "./SizesListContainer";
 
-import './App.css';
-
 import {withStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#6078E9",
+            dark: "#5C6FE9"
+        },
+        secondary: {
+            main: '#cb4a53',
+            dark: "#B6444C"
+        },
+        background: {
+            default: "#6078E9"
+        }
+    },
+});
 
 
 const styles = () => ({
+    gridRoot: {
+        height: "100%",
+        marginTop: "0",
+        marginBottom: "0"
+    },
     gridItem: {
         background: 'white',
-        height: '93vh',
+        height: '100%',
         overflow: 'scroll',
         position: 'relative',
         borderRadius: '5px',
         padding: '10px' // todo : theme unit
     },
     root: {
-        height: '100%'
+        height: '100vh',
+        width: '100vw',
+        overflow: 'hidden',
+        padding: '15px'
     }
 });
 
@@ -31,28 +55,30 @@ class App extends Component {
     render() {
 
         return (
-            <div>
-                <CssBaseline />
+            <MuiThemeProvider theme={theme}>
+                <CssBaseline/>
 
-                <form>
-                    <Grid container justify="center" spacing={16}>
+                <form className={this.props.classes.root}>
+                    <Grid container justify="center" spacing={24} className={this.props.classes.gridRoot}>
                         <Grid item sm={6} xs={12}>
                             <div className={this.props.classes.gridItem}>
-                                <Typography component="h2" variant="caption" color="primary" gutterBottom>ORIGINAL IMAGE</Typography>
+                                <Typography component="h2" variant="caption" color="primary" gutterBottom>ORIGINAL
+                                    IMAGE</Typography>
                                 <OriginalImageContainer/>
                             </div>
                         </Grid>
 
                         <Grid item sm={6} xs={12}>
                             <div className={this.props.classes.gridItem}>
-                                <Typography component="h2" variant="caption" color="primary" gutterBottom>GENERATED IMAGES</Typography>
+                                <Typography component="h2" variant="caption" color="primary" gutterBottom>GENERATED
+                                    IMAGES</Typography>
                                 <SizesListContainer/>
                             </div>
                         </Grid>
 
                     </Grid>
                 </form>
-            </div>
+            </MuiThemeProvider>
         );
     }
 }
